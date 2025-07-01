c2cd705a90c72de5064b94dec31b90da775da7fa
Josiah Welch <josiah.isaiah.welch@gmail.com>
2025-06-30 23:05:27 -0500

    removed redundant setter in panel_widgets.py

--- panel_widgets.py
+++ panel_widgets.py
@@ -34,15 +34,10 @@ class MATButton(QPushButton):
 class MiscButton(QPushButton):
     def __init__(self, name, func=None):
         super().__init__(name)
-        self.name = name
         self.func = func
 
         # Sets up button listener
         self.clicked.connect(self.pressed)
-
-    def set_name(self, name):
-        self.setText(name)
-        self.name = name
 
     def set_func(self, func):
         self.func = func
